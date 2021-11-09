f5fed4843f5b0235e188c0e9b490549348c166e7
Diego Osorio <diegoso-0989@hotmail.com>
2021-11-09 16:36:00 -0500

    Clase 6 - Metodo constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -5,6 +5,17 @@ public class Doctor {
     String name;
     String speciality;
 
+    // Método Constructor:
+    Doctor(/* parámetros */) {
+        // Instrucciones que se ejecutan al crear/instanciar
+        // un nuevo objeto con la clase Doctor...
+        System.out.println("Construyendo el objeto doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del doctor es: " + name);
+    }
+
     // Comportamientos (métodos):
     public void showName() {
         // Instrucciones...
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Diego Osorio");
         myDoctor.name = "Diego Osorio";
         myDoctor.showName();
     }
