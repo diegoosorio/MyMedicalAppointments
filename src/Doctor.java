b2733aad3c8ffd0c2a0d9522daf0d4d094e16d9e
Diego Osorio <diegoso-0989@hotmail.com>
2021-11-09 16:25:26 -0500

    Clase 5 - Creando la primera clase

---
+++ src/Doctor.java
@@ -0,0 +1,13 @@
+// Clases:
+public class Doctor {
+    // Atributos:
+    int id;
+    String name;
+    String speciality;
+
+    // Comportamientos (métodos):
+    public void showName() {
+        // Instrucciones...
+        System.out.println(name);
+    }
+}
